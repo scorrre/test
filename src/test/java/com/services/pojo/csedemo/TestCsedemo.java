566887903d9cb1442954af4e0e8ae0963fdd4354
scorrre <33421171+scorrre@users.noreply.github.com>
2017-11-08 10:34:39 +0800

    Update TestCsedemo.java

--- src/test/java/com/services/pojo/csedemo/TestCsedemo.java
+++ src/test/java/com/services/pojo/csedemo/TestCsedemo.java
@@ -56,10 +56,10 @@ public class TestCsedemo {
     @Test
     public void testsayHi(){
 
-        String expactReturnValue = "hi, tom! welcome to Servicestage!"; // You should put the expect String type value here.
+        String expactReturnValue = "hi, tom! welcome to Servicestage!!"; // You should put the expect String type value here.
 
         // You should put the parameter here, in such footprint: csedemoImpl.sayHi(String name)
-        String returnValue = csedemoImpl.sayHi("hi, tom! welcome to Servicestage!");
+        String returnValue = csedemoImpl.sayHi("tom");
 
         assertEquals(expactReturnValue, returnValue);
     }
